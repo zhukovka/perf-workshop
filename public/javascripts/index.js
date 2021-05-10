afd09991dd7746237c6e63a0a86f6c20624ba52c
zhukovka <olenazh@wix.com>
2021-05-10 18:08:46 +0300

    added Navigation Timing API PerformanceObserver

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,15 @@
-console.log("kuku");
+// Catch errors since some browsers throw when using the new `type` option.
+// https://bugs.webkit.org/show_bug.cgi?id=209216
+try {
+    // Create the performance observer.
+    const po = new PerformanceObserver((list) => {
+        for (const entry of list.getEntries()) {
+            console.log(entry)
+            console.log('Time to first byte', entry.responseStart);
+        }
+    });
+    // Start listening for `navigation` entries to be dispatched.
+    po.observe({type: 'navigation', buffered: true});
+} catch (e) {
+    // Do nothing if the browser doesn't support this API.
+}
